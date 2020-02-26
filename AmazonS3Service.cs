007343c7b012afcffa7aa4c2467fda1ade72de3b
saikumarputta <saianuram1@gmail.com>
2020-02-26 19:28:47 +0530

    first commit

---
+++ AmazonS3Service.cs
@@ -0,0 +1,64 @@
+﻿using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+using System;
+using System.Threading.Tasks;
+
+namespace downxl
+{
+    public class AmazonS3Service
+    {
+        private static string AccessKey = "AKIAJJCGG7OWRMZBKMSA";
+        private static string accessSecretKey = "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";
+        private static string bucket = "incus5-com-stg";
+        private const string filePath = @"";
+        private static readonly RegionEndpoint bucketRegion = RegionEndpoint.EUWest1;
+
+        private static IAmazonS3 client;
+
+        public static void Main()
+        
+        {
+            client = new AmazonS3Client(bucketRegion);
+            WritingAnObjectAsync().Wait();
+        }
+
+        static async Task WritingAnObjectAsync()
+        {
+            try
+            {
+                // 1. Put object-specify only key name for the new object.
+                var putRequest1 = new PutObjectRequest
+                {
+                    BucketName = bucket,
+                    Key = AccessKey,
+                    ContentBody = "sample text"
+                };
+
+                PutObjectResponse response1 = await client.PutObjectAsync(putRequest1);
+
+              //  2.Put the object-set ContentType and add metadata.
+                var putRequest2 = new PutObjectRequest
+                {
+                    BucketName = bucket,
+                    Key = accessSecretKey,
+                    FilePath = filePath,
+                    ContentType = "text/plain"
+                };
+                putRequest2.Metadata.Add("x-amz-meta-title", "someTitle");
+            }
+            catch (AmazonS3Exception e)
+            {
+                Console.WriteLine(
+                        "Error encountered ***. Message:'{0}' when writing an object"
+                        , e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(
+                    "Unknown encountered on server. Message:'{0}' when writing an object"
+                    , e.Message);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using System;
+using System.Drawing;
+using System.IO;
+using Spire.Xls;
+using Spire.Pdf;
+
+namespace downxl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExcelPackage excelPackage = new ExcelPackage();
+            ExcelWorksheet wsSheet1 = excelPackage.Workbook.Worksheets.Add("Sheet1");
+            using (ExcelRange Rng = wsSheet1.Cells[1, 1, 4, 4])
+            {
+                Rng.Value = "Report Notification Config";
+                wsSheet1.Cells[2, 1].Value = "Reporttype";
+
+                //Color colFromHex = System.Drawing.ColorTranslator.FromHtml("#B7DEE8");
+
+                //coloums
+                wsSheet1.Cells[3, 1].Value = "Name";
+                wsSheet1.Cells[3, 1].Style.Font.Bold = true;
+                // wsSheet1.Cells[3, 1][3, 2][3, 3].Style.Fill.PatternType = ExcelFillStyle.Solid;
+                // wsSheet1.Cells[3, 1][3, 2][3, 3].Style.Fill.BackgroundColor.SetColor(colFromHex);
+                wsSheet1.Cells[3, 2].Value = "EmailAddresses";
+                wsSheet1.Cells[3, 2].Style.Font.Bold = true;
+                wsSheet1.Cells[3, 3].Value = "PhoneNumber";
+                wsSheet1.Cells[3, 3].Style.Font.Bold = true;
+
+                //data
+                wsSheet1.Cells[4, 1].Value = "sai";
+                wsSheet1.Cells[4, 2].Value = "skputta@deltaintech.com";
+                wsSheet1.Cells[4, 3].Value = "7036355827";
+
+                Rng.Merge = true;
+                Rng.Style.Border.Top.Style = ExcelBorderStyle.Thin;
+                Rng.Style.Border.Left.Style = ExcelBorderStyle.Thin;
+                Rng.Style.Border.Right.Style = ExcelBorderStyle.Thin;
+                Rng.Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+            }
+            wsSheet1.Protection.IsProtected = false;
+            wsSheet1.Protection.AllowSelectLockedCells = false;
+
+            Workbook wb = new Workbook();
+            wb.LoadFromFile(@"D:\New5.xlsx");
+            wb.ConverterSetting.SheetFitToPage = true;
+            Worksheet sheet = wb.Worksheets[0];
+            for (int i = 1; i < sheet.Columns.Length; i++)
+            {
+                sheet.AutoFitColumn(i);
+            }
+            sheet.SaveToPdf(@"D:\toPDF.pdf");
+
+            //Workbook workbook = new Workbook();
+            //workbook.LoadFromFile(@"D:\New5.xlsx", ExcelVersion.Version2010);
+            //workbook.SaveToFile(@"D:\result.pdf", Spire.Xls.FileFormat.PDF);
+
+            //excelPackage.SaveAs(new FileInfo(@"D:\New5.xlsx"));
+            Console.WriteLine("File has been Downloaded...!");
+        }
+
+    }
+}
